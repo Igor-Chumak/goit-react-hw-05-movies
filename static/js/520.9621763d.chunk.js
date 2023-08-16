11b361b51733be0a75b5bc5c629598e04b03ee5d
Igor-Chumak <125488812+Igor-Chumak@users.noreply.github.com>
2023-08-16 18:40:24 +0000

    Deploying to gh-pages from @ Igor-Chumak/goit-react-hw-05-movies@0dce3e4e0c11e7772fcd73b3989f2b672644e69e 🚀

---
+++ static/js/520.9621763d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[520],{4520:function(e,r,t){t.r(r),t.d(r,{NotFoundPage:function(){return u}});var o=t(8388),n=t(184),u=function(){return(0,n.jsx)(o.jj,{textError:"Error code 404 Sorry, Page NOT FOUND !!!"})};r.default=u}}]);
+//# sourceMappingURL=520.9621763d.chunk.js.map
