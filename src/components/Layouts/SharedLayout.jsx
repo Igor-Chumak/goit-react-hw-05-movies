2983e536f3f2a8414ca3e2febb3d8794a562a135
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-08-17 10:42:30 +0300

    upgrafw Scroll Up

--- src/components/Layouts/SharedLayout.jsx
+++ src/components/Layouts/SharedLayout.jsx
@@ -2,6 +2,9 @@ import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Header, NavLinkStyled } from './SharedLayout.styled';
 import { Loader } from 'components';
+//
+import ScrollToTop from 'react-scroll-up';
+import { ScrollUp } from 'components';
 
 export const SharedLayout = () => {
   return (
@@ -17,6 +20,9 @@ export const SharedLayout = () => {
       <Suspense fallback={<Loader />}>
         <Outlet />
       </Suspense>
+      <ScrollToTop showUnder={160}>
+        <ScrollUp></ScrollUp>
+      </ScrollToTop>
     </div>
   );
 };
